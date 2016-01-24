5584e854bad09840182676ba26f1783410faaea7
Anand Nadar <anand.nadar@me.com>
2016-01-24 14:31:00 -0500

    Commiting Changes

--- cachematrix.R
+++ cachematrix.R
@@ -19,10 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if(!is.null(inv)) {
-    message("getting cached data.")
+    message("Returning cached data.")
     return(inv)
   }
   data <- x$get()
